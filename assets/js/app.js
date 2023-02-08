b2ba4e8233ed5c2790e2a83be11a51038a8626f1
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-02-07 22:25:37 -0800

    shipDetails

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,28 +20,3 @@ closeBtn.addEventListener('click', () => {
     transition: linear 200ms;
     `
 })
-
-const nextBtn = document.querySelector(".next");
-const prevBtn = document.querySelector(".prev");
-const slide = document.querySelectorAll(".slide-img");
-var i = 0;
-
-prevBtn.onclick = function () {
-    slide[i].classList.remove("active");
-    i--;
-    if (i < 0) {
-        i = slide.length - 1;
-    }
-    slide[i].classList.add("active");
-};
-
-nextBtn.onclick = function () {
-    slide[i].classList.remove("active");
-    i++;
-
-    if (i >= slide.length) {
-        i = 0;
-    }
-
-    slide[i].classList.add("active");
-};
